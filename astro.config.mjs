f1f84166cdadfa2911c255596646b08ec57b12dc
Nanae-j <nanae9577@gmail.com>
2025-04-02 16:50:34 +0900

    [update]SEO関連の修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  site: "https://astro-blog-udemy-28y.pages.dev/",
+  integrations: [preact(), sitemap()],
 });
